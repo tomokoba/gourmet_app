59947cf68071cbc733e5bc778c8faad815365b53
tomokoba <tomokoba64@gmail.com>
2021-05-02 12:30:55 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '0bb72f05619c4fcd025bfdcd0fd515c4c96179db652057658a2c51be1705174d5fc280ba6f21d44ccdcf083c16625811918cf0aa96076e5ddadb142e800e23de'
